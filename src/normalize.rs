418594b0e8b68b6d73f56b75f9fd5934d172129c
David Renshaw <dwrenshaw@gmail.com>
2015-07-03 14:02:46 -0400

    Add some Rust stuff.

---
+++ src/normalize.rs
@@ -0,0 +1,9 @@
+
+
+//! Receives an (ascii, whitespace-delimited) wordlist on stdin. Removes all words that are contained in
+//! other words. Writes the resulting word list to stdout.
+//!
+
+pub fn main() {
+    println!("hello world");
+}
